81a8bf16ff55d9dbfde3787e2d44156ae51d8b12
Jordan Wright <wright.jordan.n@outlook.com>
2022-12-01 13:10:56 -0600

    some initial boilerplate

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-export {};
+import type * as tsHTTP from "ts-http";
+
+function use(next: tsHTTP.Handler): tsHTTP.Handler {
+  return async function sessionMiddleware(req, res, ctx) {
+    await next(req, res, ctx);
+  };
+}
+
+function Session() {
+  return {
+    use,
+  };
+}
+
+export { Session };
