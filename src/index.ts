f087dfa82f3abcd6ad36741f557dcb6aafbf657c
Jordan Wright <wright.jordan.n@outlook.com>
2023-02-15 18:25:28 -0700

    export errors

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
-import type { SessionError } from "./errors.js";
+import type {
+  SessionError,
+  BadSigError,
+  SessionIDGenError,
+  StoreDeleteError,
+  StoreGetError,
+  StoreSetError,
+} from "./errors.js";
 import type { SessionData } from "./types/SessionData.js";
 import type { SessionConfig } from "./types/SessionConfig.js";
 import type { SessionMiddleware } from "./types/SessionMiddleware.js";
@@ -17,6 +24,13 @@ declare module "ts-http" {
   }
 }
 
-// TODO: export error classes
+export {
+  SessionError,
+  SessionIDGenError,
+  BadSigError,
+  StoreDeleteError,
+  StoreGetError,
+  StoreSetError,
+};
 export { SessionConfig, SessionData, SessionMiddleware, SessionStore };
 export { Session };
